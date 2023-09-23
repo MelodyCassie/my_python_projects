3ac6754b6e474e48f21b6f7a80db3f3119d233b9
Melody Oluchi <melodyoluchi848@gmail.com>
2023-09-23 10:32:57 +0100

    assignment

---
+++ assignments/for_loop_shapes.py
@@ -0,0 +1,12 @@
+for i in range(1):
+    print("*")
+    print("**")
+    print("***")
+for j in range(1):
+    print("****")
+    print("*****")
+    print("****")
+for i in range(1):
+    print("***")
+    print("**")
+    print("*")
